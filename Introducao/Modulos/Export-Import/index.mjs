97ebb7c0d3d86ddf3e267fefb11fe14dcc9dfebf
Jose <josecarlos099916@gmail.com>
2024-03-09 23:15:47 -0300

    Trabalhando com Export e Import Modules

---
+++ Introducao/Modulos/Export-Import/index.mjs
@@ -0,0 +1,6 @@
+// IMPORTANDO O MÓDULO
+
+import soma from './meu-modulo.mjs'
+
+soma(10, 10)
+soma(20, 20)
---
+++ Introducao/Modulos/Export-Import/meu-modulo.mjs
@@ -0,0 +1,7 @@
+// EXPORTANDO UM MÓDULO
+
+function soma(a, b) {
+    console.log(a + b)
+}
+
+export default soma
